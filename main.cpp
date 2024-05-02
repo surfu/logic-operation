b3e4ed2b6d12819f00232e8afe5126fb2daae00f
surfu <139760128+surfu@users.noreply.github.com>
2024-05-02 18:12:22 +0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,2 +0,0 @@
-// HM:https://materials.itstep.org/content/f3a4c780-d5c2-4c83-8927-85cafcfe9354/uk/?cache=0
-// LB:https://materials.itstep.org/content/c3f5f869-bcbf-461d-b0eb-c374e83cb4dc/uk/?cache=0
